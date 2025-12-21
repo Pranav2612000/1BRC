ccde75c449baa5dcd84f0afd9977ce35452c42f6
Pranav Joglekar <pranav2000joglekar@gmail.com>
2025-12-21 13:15:55 +0530

    feat: first basic working version

---
+++ run.sh
@@ -0,0 +1,1 @@
+cargo run data/out.txt > data/results.txt
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,81 @@
+use std::collections::HashMap;
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn parse_temperature_line(line: &str) -> (String, f32) {
+    let parts = line.split(";").collect::<Vec<&str>>();
+    let city = parts.get(0).expect("should have the city part").to_string();
+    let temperature = parts
+        .get(1)
+        .expect("should have temperature part")
+        .parse::<f32>()
+        .expect("temperature should be f32 parseable");
+
+    (city, temperature)
+}
+
+fn print_results(station_stats: HashMap<String, StationStats>) {
+    let mut results = station_stats
+        .into_iter()
+        .map(|(key, value)| return (key, value))
+        .collect::<Vec<(String, StationStats)>>();
+    results.sort_by(|a, b| (a.0).cmp(&b.0));
+
+    print!("{{");
+    for result in results {
+        print!(
+            "{}={}/{}/{}, ",
+            result.0,
+            result.1.min,
+            result.1.sum / (result.1.count as f32),
+            result.1.max,
+        );
+    }
+    print!("}}");
+}
+
+pub struct StationStats {
+    pub min: f32,
+    pub sum: f32,
+    pub max: f32,
+    pub count: u64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let data_file = args
+        .get(1)
+        .expect("data file should be passed as an argument");
+    println!("Running 1BRC on file {}", data_file);
+
+    let mut station_stats: HashMap<String, StationStats> = HashMap::new();
+
+    let file = File::open(data_file).expect("should be able to open file for reading");
+    let reader = BufReader::new(file);
+    for (i, line) in reader.lines().enumerate() {
+        if i % 100000 == 0 {
+            println!("100000 line completed");
+        }
+
+        let line = line.expect("reading a line should always suceed");
+        let (city, temperature) = parse_temperature_line(line.as_str());
+
+        station_stats
+            .entry(city)
+            .and_modify(|stats| {
+                stats.count = stats.count + 1;
+                stats.sum = stats.sum + temperature;
+                stats.max = f32::max(stats.max, temperature);
+                stats.min = f32::min(stats.min, temperature);
+            })
+            .or_insert(StationStats {
+                min: temperature,
+                sum: temperature,
+                max: temperature,
+                count: 1,
+            });
+    }
+
+    print_results(station_stats);
 }
