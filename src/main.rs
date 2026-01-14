d1bee967096e581f75a114f58fa4a05918aa231c
Pranav Joglekar <pranav2000joglekar@gmail.com>
2026-01-14 12:03:43 +0530

    feat: run multiple computation threads

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ use tracing_subscriber::fmt::format::FmtSpan;
 
 const DEFAULT_BUFFER_SIZE: usize = 16 * 1024 * 1024;
 const CHANNEL_SIZE: usize = 2 * 1024 * 1024;
+const DEFAULT_NUM_THREADS: usize = 4;
 
 fn parse_temperature_line(line: &str) -> (String, f32) {
     let parts = line.split(";").collect::<Vec<&str>>();
@@ -48,11 +49,33 @@ fn print_results(station_stats: HashMap<String, StationStats>, mut out_fd: &mut 
     write!(&mut out_fd, "}}").expect("write to output file should suceed");
 }
 
+#[derive(Clone)]
 pub struct StationStats {
     pub min: f32,
     pub sum: f32,
     pub max: f32,
     pub count: u64,
+}
+
+fn merge_results(
+    result: &mut HashMap<String, StationStats>,
+    partial_results: &HashMap<String, StationStats>,
+) {
+    for entry in partial_results.iter() {
+        let city = entry.0;
+        let station_stats = entry.1;
+
+        result
+            .entry(city.clone())
+            .and_modify(|stats| {
+                stats.count = stats.count + station_stats.count;
+                stats.sum = stats.sum + station_stats.sum;
+                stats.max = f32::max(stats.max, station_stats.max);
+                stats.min = f32::min(stats.min, station_stats.min);
+            })
+            .or_insert(station_stats.clone());
+    }
+    return;
 }
 
 fn main() {
@@ -62,8 +85,15 @@ fn main() {
         .expect("data file should be passed as an argument")
         .clone();
     let out_file = args.get(2);
+    let num_threads = DEFAULT_NUM_THREADS;
+    // let num_threads = std::thread::available_parallelism()
+    //     .map(|s| s.get() - 4)
+    //     .unwrap_or(DEFAULT_NUM_THREADS);
 
-    println!("Running 1BRC on file {}", data_file);
+    println!(
+        "Running 1BRC on file {} with {} threads",
+        data_file, num_threads
+    );
     fmt::fmt()
         .with_span_events(FmtSpan::CLOSE)
         .with_target(false)
@@ -75,7 +105,7 @@ fn main() {
     let (sender, receiver) = crossbeam_channel::bounded::<String>(CHANNEL_SIZE);
     let mut station_stats: HashMap<String, StationStats> = HashMap::new();
 
-    let producer_handle = std::thread::spawn(move || {
+    std::thread::spawn(move || {
         let file = File::open(data_file).expect("should be able to open file for reading");
         let mut reader = BufReader::with_capacity(DEFAULT_BUFFER_SIZE, file);
 
@@ -95,22 +125,37 @@ fn main() {
         }
     });
 
-    for line in receiver.iter() {
-        let (city, temperature) = parse_temperature_line(line.as_str());
-        station_stats
-            .entry(city)
-            .and_modify(|stats| {
-                stats.count = stats.count + 1;
-                stats.sum = stats.sum + temperature;
-                stats.max = f32::max(stats.max, temperature);
-                stats.min = f32::min(stats.min, temperature);
-            })
-            .or_insert(StationStats {
-                min: temperature,
-                sum: temperature,
-                max: temperature,
-                count: 1,
-            });
+    let mut threads = Vec::with_capacity(num_threads);
+    for _i in 0..num_threads {
+        let rx = receiver.clone();
+        let handle = std::thread::spawn(move || {
+            let mut station_stats: HashMap<String, StationStats> = HashMap::new();
+            for line in rx.iter() {
+                let (city, temperature) = parse_temperature_line(line.as_str());
+                station_stats
+                    .entry(city)
+                    .and_modify(|stats| {
+                        stats.count = stats.count + 1;
+                        stats.sum = stats.sum + temperature;
+                        stats.max = f32::max(stats.max, temperature);
+                        stats.min = f32::min(stats.min, temperature);
+                    })
+                    .or_insert(StationStats {
+                        min: temperature,
+                        sum: temperature,
+                        max: temperature,
+                        count: 1,
+                    });
+            }
+
+            return station_stats;
+        });
+        threads.push(handle);
+    }
+
+    for handle in threads {
+        let partial_station_stats = handle.join().expect("Thread panicked");
+        merge_results(&mut station_stats, &partial_station_stats);
     }
 
     if let Some(out_file) = out_file {
